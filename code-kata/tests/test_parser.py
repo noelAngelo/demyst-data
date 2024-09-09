1e63368c9c675a542914437fe61e1e475f9bb8fd
Noel <noel.angelo.borneo@gmail.com>
2024-09-09 22:40:55 +0000

    added unit test base

---
+++ code-kata/tests/test_parser.py
@@ -0,0 +1,12 @@
+import pytest
+
+class TestParser:
+
+    def test_generated_file_exists(self):
+        pass
+
+    def test_parser_field_length(self):
+        pass
+
+    def test_parser_special_characters(self):
+        pass
